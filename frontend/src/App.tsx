85b6452c8e0772bf60dcd7483729f43f5239d6ea
lautaroeberle <louloufutbol@gmail.com>
2025-07-01 14:13:05 -0300

    mejoro hero y navbar, emprolijo todo

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,10 @@
-import Navbar from './components/Navbar';
+
 import Hero from './components/Hero';
-import Beneficios from './components/Beneficios';
-import Sedes from './components/Sedes';
-import Inscripcion from './components/Inscripcion';
-import Contacto from './components/Contacto';
 
 function App() {
   return (
     <>
-      <Navbar />
       <Hero />
-      <Beneficios />
-      <Sedes />
-      <Inscripcion />
-      <Contacto />
     </>
   );
 }
