f8d41222c548e289b28dbd1e3d0719761ccb5745
lautaroeberle <louloufutbol@gmail.com>
2025-07-01 14:32:54 -0300

    agrego transparencia navbar y cambio titulo index.html

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,24 @@
-
+import { useEffect } from "react";
 import Hero from './components/Hero';
 
 function App() {
+
+   useEffect(() => {
+    const navbar = document.querySelector(".navbar");
+
+    const handleScroll = () => {
+      if (window.scrollY > 80) {
+        navbar?.classList.add("transparent");
+      } else {
+        navbar?.classList.remove("transparent");
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+
   return (
     <>
       <Hero />
