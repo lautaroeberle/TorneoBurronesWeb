7b072bc6cf8bab525d4efc4447893ff72bf1a3eb
lautaroeberle <louloufutbol@gmail.com>
2025-07-04 15:36:03 -0300

    corrijo navbar molesto

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,9 +1,27 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import NavLinkHero from "./NavLinkHero";
 
 const Navbar = () => {
+  const [isVisible, setIsVisible] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
+
+  const handleScroll = () => {
+    if (window.scrollY > lastScrollY) {
+      setIsVisible(false); // scrolleando hacia abajo
+    } else {
+      setIsVisible(true); // hacia arriba
+    }
+    setLastScrollY(window.scrollY);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [lastScrollY]);
+
   return (
-    <nav className="navbar">
+    <nav className={`navbar ${!isVisible ? "navbar-hidden" : ""}`}>
       <ul>
         <li><NavLinkHero to="inicio">Inicio</NavLinkHero></li>
         <li><NavLinkHero to="beneficios">Beneficios</NavLinkHero></li>
@@ -11,8 +29,6 @@ const Navbar = () => {
         <li><NavLinkHero to="inscripcion">Inscripción</NavLinkHero></li>
         <li><NavLinkHero to="contacto">Contacto</NavLinkHero></li>
         <li><Link to="/reglamento">Reglamento</Link></li>
-
-
         <li className="dropdown">
           <span>Torneos ▾</span>
           <ul className="dropdown-menu">
