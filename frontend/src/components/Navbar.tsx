02e1639415fe6bc1a30e73aaf1ce7b9e64f8e3be
lautaroeberle <louloufutbol@gmail.com>
2025-07-21 20:59:29 -0300

    modifico y mejoro el navbar. elimino estilos globalcss innecesarios

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,6 +1,9 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import NavLinkHero from "./NavLinkHero";
+import { FaInstagram} from "react-icons/fa";
+import "../styles/navbar.css";
+import LOGO from "../assets/logo.png";
 
 const Navbar = () => {
   const [isVisible, setIsVisible] = useState(true);
@@ -8,7 +11,7 @@ const Navbar = () => {
 
   const handleScroll = () => {
     if (window.scrollY > lastScrollY) {
-      setIsVisible(false); // scrolleando hacia abajo
+      setIsVisible(false); // hacia abajo
     } else {
       setIsVisible(true); // hacia arriba
     }
@@ -22,21 +25,48 @@ const Navbar = () => {
 
   return (
     <nav className={`navbar ${!isVisible ? "navbar-hidden" : ""}`}>
-      <ul>
-        <li><NavLinkHero to="inicio">Inicio</NavLinkHero></li>
-        <li><NavLinkHero to="beneficios">Beneficios</NavLinkHero></li>
-        <li><NavLinkHero to="inscripcion">Inscripción</NavLinkHero></li>
-        <li><NavLinkHero to="contacto">Contacto</NavLinkHero></li>
-        <li><Link to="/reglamento">Reglamento</Link></li>
-        <li><Link to = "/noticias">Periodismo</Link>"</li>
-        <li className="dropdown">
-          <span>Torneos ▾</span>
-          <ul className="dropdown-menu">
-            <li><Link to="/copa">Copa</Link></li>
-            <li><Link to="/apertura">Apertura</Link></li>
-          </ul>
-        </li>
-      </ul>
+      <div className="navbar-container">
+        <div className="navbar-left">
+          <Link to="/" className="logo">
+            <img src={LOGO} alt="Logo TIBA" className="logo-img" />
+            <span className="tiba-text">TIBA</span>
+          </Link>
+        </div>
+
+        <ul className="navbar-menu">
+          <li><NavLinkHero to="inscripcion">Inscripción</NavLinkHero></li>
+          <li><Link to="/reglamento">Reglamento</Link></li>
+          <li><Link to="/noticias">Noticias</Link></li>
+          <li className="dropdown">
+            <span>Torneos ▾</span>
+            <ul className="dropdown-menu">
+              <li><Link to="/copa">Copa</Link></li>
+              <li><Link to="/apertura">Apertura</Link></li>
+            </ul>
+          </li>
+        </ul>
+
+        <div className="navbar-right">
+          <a
+            href="https://wa.me/5491141723184"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="whatsapp-text"
+            title="WhatsApp"
+          >
+            +5491141723182
+          </a>
+          <a
+            href="https://instagram.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="icon-button"
+            title="Instagram"
+          >
+            <FaInstagram />
+          </a>
+        </div>
+      </div>
     </nav>
   );
 };
